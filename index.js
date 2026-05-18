f1d502db099ddcd538e9f55e8c68acbd2f917329
Your Name <babulhossan.info@gmail.com>
2026-05-19 01:38:38 +0600

    daynamic booking route

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const PORT = process.env.PORT || 5000; // PORT এর ব্যাকআপ হিসেবে 5000 রাখা হলো
+const PORT = process.env.PORT || 5000;
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -77,6 +77,25 @@ async function run() {
         })
 
 
+        app.patch('/tutor/:id/decrease-slot', async (req, res) => {
+            try {
+                const id = req.params.id;
+                const filter = { _id: new ObjectId(id) };
+
+
+                const updateDoc = {
+                    $inc: { totalSlot: -1 }
+                };
+
+                const result = await tutorCollection.updateOne(filter, updateDoc);
+                res.status(200).json(result);
+            } catch (error) {
+                console.error("Decrease slot error:", error);
+                res.status(500).json({ error: "Failed to decrease slot or invalid ID" });
+            }
+        });
+
+
         app.post('/add-booking', async (req, res) => {
             try {
                 const bookingData = req.body;
@@ -99,11 +118,24 @@ async function run() {
             }
         });
 
+        app.patch('/booking/:bookingId', async (req, res) => {
+            const { bookingId } = req.params
+
+
+            const result = await bookingCollection.updateOne(
+                { _id: new ObjectId(bookingId) },
+                { $set: { status: 'cancelled' } }
+            )
+
+            res.json(result)
+        })
+
         console.log("Successfully connected to MongoDB!");
     } catch (error) {
         console.error("MongoDB Connection Error:", error);
     }
 }
+
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
